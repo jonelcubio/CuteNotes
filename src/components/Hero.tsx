f391696ae9f8ef6d5f5a3f8536c93e0eeb2bec50
Jonelle Cubio <girlthatloveswriting@gmail.com>
2026-02-11 14:41:13 +0800

    📱 Added the login to post a note

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,24 @@
+import lock from "../assets/lock.svg";
+import Notes from "./Notes";
 export default function Hero(){
   return(
     <>
     <div className="hero-main-con">
       <div className="hero-main">
         <div className="hero-landing">Capture Your<span className="hero-landing-span"> Happy </span>Thoughs</div>
-        <div className="header-left-text">A cozy corner of the internet for your daily musings,<br className="header-left-br"/>big ideas, and little reminders.</div>
-      </div>
+        <div className="header-left-text">A cozy corner of the internet for your daily musings,<br className="header-left-br"/>big ideas, and little reminders.</div>   
+    </div>
+     
+     <div className="header-btn-btn">
+        <div className="header-btn-con">
+          <img src={lock} alt="Login to Post a Note" className="header-btn-img" />
+          <div className="header-btn-text">Login to Post a Note</div>
+        </div>
+     </div>
+    
+      <Notes />
+     
+
     </div>
     </>
 
