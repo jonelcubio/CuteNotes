7f1bf71a98948ec787b2af7540af9fe27a3382f4
Jonelle Cubio <girlthatloveswriting@gmail.com>
2026-02-11 14:11:58 +0800

    🎪 Added Click Spark cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import Home from "./pages/Home"
+import ClickSpark from "./ReactBits/ClickSpark"
 
 function App() {
 
   return (
     <>
+  <ClickSpark
+      sparkColor='black'
+      sparkSize={10}
+      sparkRadius={15}
+      sparkCount={8}
+      duration={400}
+    >
       <Home />
+  </ClickSpark>
     </>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,7 @@ export default function Hero(){
     <div className="hero-main-con">
       <div className="hero-main">
         <div className="hero-landing">Capture Your<span className="hero-landing-span"> Happy </span>Thoughs</div>
+        <div className="header-left-text">A cozy corner of the internet for your daily musings,<br className="header-left-br"/>big ideas, and little reminders.</div>
       </div>
     </div>
     </>
