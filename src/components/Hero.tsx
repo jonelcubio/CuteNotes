c1a22b98919318c5c5d196b0318b050c3c3f7bb0
Jonelle Cubio <girlthatloveswriting@gmail.com>
2026-02-11 13:50:32 +0800

    ☕️ Replaced the default font

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero(){
+  return(
+    <>
+    <div className="hero-main-con">
+      <div className="hero-main">
+        <div className="hero-landing">Capture Your<span className="hero-landing-span"> Happy </span>Thoughs</div>
+      </div>
+    </div>
+    </>
+
+  );
+}
