728a4582fbed9af36b2c2903e09e06e006535d08
Jonelle Cubio <girlthatloveswriting@gmail.com>
2026-02-19 13:32:32 +0800

    🎃 added the feature/login branch

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://fszgkzoircomczitjxdr.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzemdrem9pcmNvbWN6aXRqeGRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5OTA3MTIsImV4cCI6MjA4NjU2NjcxMn0.ER43SzJgETotKcZUPrmQWcqcfjaab2zYLhs3vKLJ7jQ'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
